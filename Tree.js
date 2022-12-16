f096c7e9007ca6429bb87161c8faaf155259cde2
Ramin <farhadiramin91@gmail.com>
2022-12-15 23:46:48 -0500

    insert & lookup method  was added to the tree

--- Tree.js
+++ Tree.js
@@ -38,17 +38,55 @@ class BinarySearchTree {
       }
     }
   }
+  lookup(value) {
+    if (this.root !== null) {
+      if (this.root.value === value) {
+        return true;
+      }
+      let parent = this.root;
+
+      while (parent.left !== null || parent.right !== null) {
+        if (parent.value === value) {
+          return true;
+        } else {
+          while (value > parent.value) {
+            if (parent === null) {
+              return false;
+            }
+            parent = parent.right;
+            if (!parent) {
+              return false;
+            }
+            if (parent.value === value) {
+              return true;
+            }
+          }
+          while (value < parent.value) {
+            parent = parent.left;
+            if (!parent) {
+              return false;
+            }
+            if (parent.value === value) {
+              return true;
+            }
+          }
+        }
+      }
+      return false;
+    } else {
+      return false;
+    }
+  }
 }
-//   lookup(value) {}
 //remove
-// }
 
 const tree = new BinarySearchTree();
-tree.insert(10);
-tree.insert(100);
-tree.insert(95);
-tree.insert(96);
-tree.insert(98);
-tree.insert(97);
+tree.insert(9);
+tree.insert(4);
+tree.insert(6);
+tree.insert(20);
+tree.insert(170);
+tree.insert(15);
+tree.insert(1);
 
 console.log(tree);
